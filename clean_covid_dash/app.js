cf444f8462cac18af72f0ab823a32cb3e68d87ae
EmmaNeuberger <54480530+EmmaNeuberger@users.noreply.github.com>
2020-04-14 16:28:33 -0600

    updated files with maps and chat.js

--- clean_covid_dash/app.js
+++ clean_covid_dash/app.js
@@ -118,6 +118,85 @@ function plotCurrent (stateSel) {
 
         Plotly.newPlot("combineddata", combinedData, combinedLayout);
 
+        // Map attempt
+
+        var stateList = []
+        var caseList = []
+
+        for (i = 0; i < currentStates.length; i++) {
+            state = currentStates[i];
+            stateList.push(state);
+        };
+
+        for (i = 0; i < totalCases.length; i++) {
+            cases = totalCases[i] / 800;
+            caseList.push(cases);
+        }
+
+        var mapData = [{
+            type: 'scattergeo',
+            locationmode: 'USA-states',
+            locations: stateList ,
+            hoverinfo: 'text',
+            text: stateList,
+            marker: {
+                size: caseList,
+                line: {
+                    color: 'black',
+                    width: 2
+                },
+            }
+        }];
+    
+        var mapLayout = {
+            title: 'US Cases Bubble',
+            showlegend: false,
+            geo: {
+                scope: 'usa',
+                projection: {
+                    type: 'albers usa'
+                },
+                showland: true,
+                landcolor: 'rgb(217, 217, 217)',
+                subunitwidth: 1,
+                countrywidth: 1,
+                subunitcolor: 'rgb(255,255,255)',
+                countrycolor: 'rgb(255,255,255)'
+            },
+            height: 1000,
+            width: 1000
+        }
+    
+        Plotly.newPlot("bubblemap", mapData, mapLayout, {showLink: false});
+
+                
+        var cloroData = [{
+            type: 'choropleth',
+            locationmode: 'USA-states',
+            locations: stateList,
+            z: totalCases,
+            text: stateList,
+            autocolorscale: true
+        }];
+
+        var cloroLayout = {
+        title: 'Cloro Cases',
+            geo:{
+                scope: 'usa',
+                countrycolor: 'rgb(255, 255, 255)',
+                showland: true,
+                landcolor: '#27608a',
+                showlakes: true,
+                lakecolor: 'rgb(255, 255, 255)',
+                subunitcolor: 'rgb(255, 255, 255)',
+                lonaxis: {},
+                lataxis: {}
+            },
+            height: 1000,
+            width: 1000
+        };
+        Plotly.newPlot("colormap", cloroData, cloroLayout, {showLink: false})
+
 })
 }
 
@@ -175,6 +254,41 @@ function plotHistorical (stateSel) {
     })
 }
 
+function graphjs(stateSel) {
+
+    var url = "https://covidtracking.com/api/v1/states/current.json";
+
+    d3.json(url).then(data => {
+
+        var currentStates = data.map(x => x.state);
+        var totalTests = data.map(x => x.totalTestResults);
+        var totalCases = data.map(x => x.positive);
+        var totalHospitalized = data.map(x => x.hospitalized);
+
+        var ctx = document.getElementById('mychart');
+
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'bar',
+
+            // The data for our dataset
+            data: {
+                labels: currentStates,
+                datasets: [{
+                    label: 'Playing with Chart.js',
+                    backgroundColor: '#5cc19e',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: totalCases, totalTests, totalHospitalized
+                }]
+        },
+
+        // Configuration options go here
+        options: {}
+    })
+});
+}
+
 // Default plot: Colorado
 plotCurrent ('AK')
 plotHistorical ('AK')
+graphjs('AK')
